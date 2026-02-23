2fd3c2cc471721eaa6163809010e9c9b03cc2e71
Dominique M. <dominiquecastro81@gmail.com>
2026-02-23 02:43:47 -0600

    Implement N-Queens problem solution

---
+++ C++/Recursividad/N-Queens.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+bool NQueens(int n) {
+  int** tablero = int*[n];
+  for (int i = 0; i < n; i++) {
+    int* tablero = int[n];
+  }
+  if (solve(tablero, 0)) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        std::cout << tablero[i]
+      }
+    }
+}
+
+bool solve(int** tablero, int columna) {
+  if (
+}
+
+bool esSeguro(int** tablero, ) {
+    for (int i = 0; i < 
+}
+  
