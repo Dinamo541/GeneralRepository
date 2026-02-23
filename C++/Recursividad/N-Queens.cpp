6fa4eb5f9d1b7b180ca8598635fd234ffd42d999
Dinamo541 <dominiquecastro81@gmail.com>
2026-02-23 08:04:27 -0600

    feat: implement initial structure for N-Queens solution in C++ and Java

--- C++/Recursividad/N-Queens.cpp
+++ C++/Recursividad/N-Queens.cpp
@@ -1,23 +1,5 @@
 #include <iostream>
 
-bool NQueens(int n) {
-  int** tablero = int*[n];
-  for (int i = 0; i < n; i++) {
-    int* tablero = int[n];
-  }
-  if (solve(tablero, 0)) {
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        std::cout << tablero[i]
-      }
-    }
+bool isNQueens() {
+    
 }
-
-bool solve(int** tablero, int columna) {
-  if (
-}
-
-bool esSeguro(int** tablero, ) {
-    for (int i = 0; i < 
-}
-  
---
+++ Java/N-Queens/src/MainOne.java
@@ -0,0 +1,11 @@
+
+public class MainOne {
+    public static boolean NQueens() {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
